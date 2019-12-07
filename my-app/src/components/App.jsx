e75fccf0da074fadb46f0577011ce70dd0b3bbcf
emr4706 <emr4706@gmail.com>
2019-12-07 11:50:21 +0100

    add arrow function

--- my-app/src/components/App.jsx
+++ my-app/src/components/App.jsx
@@ -2,16 +2,16 @@ import React from "react";
 import Enter from "./Enter";
 import emojipedia from "../emojipedia";
 
-function enterCreate(emoji) {
-  return (
-    <Enter
-      key={emoji.id}
-      img={emoji.emoji}
-      name={emoji.name}
-      description={emoji.meaning}
-    />
-  );
-}
+// function enterCreate(emoji) {
+//   return (
+//     <Enter
+//       key={emoji.id}
+//       img={emoji.emoji}
+//       name={emoji.name}
+//       description={emoji.meaning}
+//     />
+//   );
+// }
 
 function App() {
   return (
@@ -19,7 +19,17 @@ function App() {
       <h1>
         <span>emojipedia</span>
       </h1>
-      <dl className="dictionary">{emojipedia.map(enterCreate)}</dl>
+      {/* <dl className="dictionary">{emojipedia.map(enterCreate)}</dl> */}
+      <dl className="dictionary">
+        {emojipedia.map(emoji => (
+          <Enter
+            key={emoji.id}
+            img={emoji.emoji}
+            name={emoji.name}
+            description={emoji.meaning}
+          />
+        ))}
+      </dl>
     </div>
   );
 }
